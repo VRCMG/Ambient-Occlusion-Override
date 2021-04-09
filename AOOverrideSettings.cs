5604063625091ca39574a6f1de3772eee06a71eb
Xerolide <82176668+Xerolide@users.noreply.github.com>
2021-04-09 11:40:49 +0000

    Update AOOverrideSettings.cs

--- AOOverrideSettings.cs
+++ AOOverrideSettings.cs
@@ -1,43 +1,33 @@
 using MelonLoader;
 using System.Reflection;
 
-namespace PostProcessingOverrides
+namespace AOOverride
 {
-    public static class PostProcessingOverridesSettings
+    public static class AOOverrideSettings
     {
-        public static float AmbientOcclusion = 0f;
-        public static float Bloom = 0f;
-        public static bool ACESTonemapping = false;
-        public static float Temperature = 0f;
-        public static float Exposure = 0f;
-        public static float Saturation = 0f;
-        public static float Contrast = 0f;
+        public static float AmbientOcclusion = 0.5f;
+        public static bool EnableAO = true;
         public static void RegisterSettings()
         {
             // Register mod settings
-            MelonPrefs.RegisterCategory("PostProcessingOverrides", "Post-processing overrides");
+            MelonPrefs.RegisterCategory("AOOverride", "Ambient occlusion overrides");
 
-            MelonPrefs.RegisterFloat("PostProcessingOverrides", nameof(AmbientOcclusion), 10f, "Ambient occlusion (0.5 recommended)");
-            MelonPrefs.RegisterFloat("PostProcessingOverrides", nameof(Bloom), 0f, "Bloom (0.1 recommended)");
-            MelonPrefs.RegisterBool("PostProcessingOverrides", nameof(ACESTonemapping), false, "ACES tonemapping");
-            MelonPrefs.RegisterFloat("PostProcessingOverrides", nameof(Temperature), 0f, "Temperature");
-            MelonPrefs.RegisterFloat("PostProcessingOverrides", nameof(Exposure), 0f, "Exposure");
-            MelonPrefs.RegisterFloat("PostProcessingOverrides", nameof(Saturation), 0f, "Saturation");
-            MelonPrefs.RegisterFloat("PostProcessingOverrides", nameof(Contrast), 0f, "Contrast");
+            MelonPrefs.RegisterFloat("AOOverride", nameof(AmbientOcclusion), 0.5f, "Ambient occlusion (0.5 recommended)");
+            MelonPrefs.RegisterBool("AOOverride", nameof(EnableAO), true, "Enable AO");
 
             OnModSettingsApplied();
         }
 
         public static void OnModSettingsApplied()
         {
-            foreach (var fieldInfo in typeof(PostProcessingOverridesSettings).GetFields(BindingFlags.Static | BindingFlags.Public))
+            foreach (var fieldInfo in typeof(AOOverrideSettings).GetFields(BindingFlags.Static | BindingFlags.Public))
             {
                 if (fieldInfo.FieldType == typeof(int))
-                    fieldInfo.SetValue(null, MelonPrefs.GetInt("PostProcessingOverrides", fieldInfo.Name));
+                    fieldInfo.SetValue(null, MelonPrefs.GetInt("AOOverride", fieldInfo.Name));
                 if (fieldInfo.FieldType == typeof(bool))
-                    fieldInfo.SetValue(null, MelonPrefs.GetBool("PostProcessingOverrides", fieldInfo.Name));
+                    fieldInfo.SetValue(null, MelonPrefs.GetBool("AOOverride", fieldInfo.Name));
                 if (fieldInfo.FieldType == typeof(float))
-                    fieldInfo.SetValue(null, MelonPrefs.GetFloat("PostProcessingOverrides", fieldInfo.Name));
+                    fieldInfo.SetValue(null, MelonPrefs.GetFloat("AOOverride", fieldInfo.Name));
             }
         }
     }
